6bce256f166a3060803743592a245339e8c9b19f
Andreas Rasmussen <andreasrefras@gmail.com>
2017-12-04 23:11:59 +0100

    Added +/- to AccordioItem

--- src/components/AccordionItem.vue
+++ src/components/AccordionItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <span @click="showText = !showText">{{ item.title }}</span>
+    <span @click="showText = !showText" :class="{ expanded: showText }">{{ item.title }}</span>
     <p v-show="showText" v-html="item.text" />
   </li>
 </template>
@@ -31,13 +31,27 @@
   }
   span {
     @extend .button;
+    position: relative;
     width: 100%;
     background-color: $color-light;
     margin-top: 1px;
     text-align: left;
     font-weight: bold;
+    line-height: 1.3;
     font-size: $fontSize-large;
     padding: $scale-2-1;
+    padding-right: $scale-6-1;
+    &:after {
+      content: '+';
+      position: absolute;
+      font-size: 40px;
+      font-weight: 100;
+      color: lighten($color-darkblue-lighter-4, 9%);
+      right: $scale-2-1;
+      top: 50%;
+      transform: translateY(-54%);
+    }
+    &.expanded:after { content: '-'; right: 21px; transform: translateY(-56%); }
     &:hover {
       background-color: darken($color-light, 2%);
     }
