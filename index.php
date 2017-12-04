9014578b3a0cde470294275c6d84024179445298
Andreas Rasmussen <andreasrefras@gmail.com>
2017-12-04 23:34:51 +0100

    Added Favicon(s)

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
+  <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/dist/assets/images/favicon.ico">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/dist/assets/images/favicon-16x16.png" sizes="16x16" />
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/dist/assets/images/favicon-32x32.png" sizes="32x32" />
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/dist/assets/images/apple-touch-icon.png">
+  <link rel="mask-icon" href="<?php bloginfo('template_directory'); ?>/dist/assets/images/safari-pinned-tab.svg" color="#5bbad5">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <meta property="og:url"                content="http://www.kommunernes-teknologispring.dk/" />
   <meta property="og:title"              content="Kommunernes teknologiske fremtid - et værktøj til viden og dialog" />
